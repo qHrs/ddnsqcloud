51981c9b9b9adc0ca03de1d87026c8c9ca86b517
qpand <qpanda@126.com>
2020-03-23 11:47:37 +0800

    修改加密方式

--- ddnsqcloud.py
+++ ddnsqcloud.py
@@ -20,20 +20,26 @@ from hashlib import sha256
 
 secretId = ""
 secretKey = ""
+endpoint = "cns.api.qcloud.com/v2/index.php"
 
 def getSignature(url):
-    appsecret = secretKey.encode('utf-8')  # 秘钥
     data = "GET" + url  # 加密数据
-    signature = base64.b64encode(hmac.new(appsecret, data.encode("utf8"), hashlib.sha256).digest())
+    signature = base64.b64encode(hmac.new(secretKey.encode('utf-8'), data.encode("utf8"), hashlib.sha256).digest())
     return signature
 
+def getSignStr(data, endpoint):
+    s = endpoint + "?"
+    #query_str = "&".join("%s=%s" % (k, params[k]) for k in sorted(params))
+    argDict = sorted(data.items(), key=lambda data:data[0])
+    queryStr = urllib.parse.urlencode(argDict)
+    return s + queryStr
 
 # 获取IPV6地址 适用于 linux系统
 def getIPAddress():
-   getIPV6_process = subprocess.Popen("ifconfig | grep global | awk '{print $2}'", shell = True, stdout = subprocess.PIPE)
-   output = (getIPV6_process.stdout.read())
-   ipv6_pattern='(([a-f0-9]{1,4}:){7}[a-f0-9]{1,4})'
-   m = re.search(ipv6_pattern, str(output))
+   process = subprocess.Popen("ifconfig | grep global | awk '{print $2}'", shell=True, stdout=subprocess.PIPE)
+   output = (process.stdout.read())
+   pattern = '(([a-f0-9]{1,4}:){7}[a-f0-9]{1,4})'
+   m = re.search(pattern, str(output))
    if m:
        return m.group()
    else:
@@ -46,6 +52,8 @@ def main():
     nonce = random.randint(1000, 9999)
     ipv6Addr = '2409:8a62:287:9ab0:b128:51fd:fd4c:d4e3' # getIPAddress()
 
+    '''
+    获取解析记录列表
     data = {'SecretId': secretId,
               'Region': 'ap-chengdu',
               'Timestamp': timestamp,
@@ -55,11 +63,10 @@ def main():
               'offset': '0',
               'length': '20',
               'domain': 'qpanda.vip'}
+'''
 
-    '''
-    获取解析记录列表
     data = {
-        'SecretId': secretId,
+              'SecretId': secretId,
               'Region': 'ap-chengdu',
               'Timestamp': timestamp,
               'Nonce': nonce,
@@ -72,13 +79,11 @@ def main():
               'recordLine': '默认',
               'value': ipv6Addr
     }
-'''
-    argDict = sorted(data.items(), key=lambda data : data[0])
-    argUrl = urllib.parse.urlencode(argDict)
 
-    getUrl = "cns.api.qcloud.com/v2/index.php?" + argUrl
-    signature = getSignature(getUrl)
-    getUrl = "https://" + getUrl + "&Signature=" + parse.quote(signature)
+
+    dataUrl = getSignStr(data, endpoint)
+    signature = getSignature(dataUrl)
+    getUrl = "https://" + dataUrl + "&Signature=" + parse.quote(signature)
     print(getUrl)
 
     response = urllib.request.urlopen(getUrl)
